39923e894ab91949aa95eeb80a67085b3d13c88a
Ahmad Alqaisi <ahmedhamed1312@gmail.com>
2022-09-20 21:51:28 +0300

    configuration for the app

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Container from "./components/Container/Container.component";
+import Footer from "./components/Footer/Footer.component";
+function App() {
+  return (
+    <div className="App">
+      <Container/>
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
