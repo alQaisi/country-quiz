beb2c112ec511e0f69144f1e9d0394107c1cf4c7
Ahmad Alqaisi <ahmedhamed1312@gmail.com>
2022-10-21 19:28:39 +0300

    disable react strict mode

--- src/index.js
+++ src/index.js
@@ -9,13 +9,11 @@ import * as serviceWorker from './serviceWorkerRegistration';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
-  <React.StrictMode>
     <CountriesProvider>
       <QuizProvider>
         <App />
       </QuizProvider>
     </CountriesProvider>
-  </React.StrictMode>
 );
 serviceWorker.register();
 
