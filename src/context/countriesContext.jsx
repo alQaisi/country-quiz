081d946c4db62fbfaf54f10db193add4b64ee7d2
Ahmad Alqaisi <ahmedhamed1312@gmail.com>
2022-10-21 19:29:00 +0300

    installing & using axios for fetching

--- src/context/countriesContext.jsx
+++ src/context/countriesContext.jsx
@@ -1,31 +1,12 @@
-import { createContext, useEffect, useState } from "react";
+import { createContext } from "react";
+import useAxios from "../utlis/useAxios";
 
 export const CountriesContext=createContext({});
 
 export function CountriesProvider({children}){
 
-    const [countries,setCountries]=useState([]);
-    const [isError,setIsError]=useState(false);
+    const { countries,isError }=useAxios();
 
-    useEffect(()=>{
-        const abortController=new AbortController();
-        const fetchData=async()=>{
-            try{
-                setIsError(false);
-                const response=await fetch("https://restcountries.com/v3.1/all");
-                const data=await response.json();
-                setCountries(data);
-            }catch(err){
-                if(!abortController.signal.aborted){
-                    setIsError(true);
-                }
-            }
-        }
-        fetchData();
-        return ()=>{
-            abortController.abort();
-        }
-    },[]);
     const value={countries,isError};
     return(
         <CountriesContext.Provider value={value}> { children }</CountriesContext.Provider>
